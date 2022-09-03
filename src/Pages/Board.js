425a77002fa8f6fcaecc1baa97d6571df6bed815
Anton Asela <to.anton.asela@gmail.com>
2022-09-03 23:59:27 +0800

    try with useState

--- src/Pages/Board.js
+++ src/Pages/Board.js
@@ -1,17 +1,33 @@
 import Table from 'react-bootstrap/Table';
 import Guess from "./Guess";
+import { useState } from 'react';
 
 function Board(){
 
+    const [guesses, setGuesses] = useState([{
+        Id: "1"
+    }, {
+        Id: "2"
+    }, {
+        Id:"3"
+    }, {
+        Id:"4"
+    }, {
+        Id:"5"
+    }, {
+        Id:"6"
+    }]);
+
     return (
+
         <Table striped bordered hover>
             <tbody>
-            <Guess/>
-            <Guess/>
-            <Guess/>
-            <Guess/>
-            <Guess/>
-            <Guess/>
+            <div> {
+            this.state.guesses.map(function (guess) {
+               return  (<Guess key={guess.Id}></Guess>)
+            })
+            }
+        </div>
             </tbody>
         </Table>
         );
